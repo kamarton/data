cc6b8f60587a28210f3dfba732a5336f6f027347
kamarton <kamarton@gmail.com>
2019-10-01 06:08:10 +0200

    Merge remote-tracking branch 'upstream/master' into 7-filter-processor-idea #7
    # Conflicts:
    #	README.md
    #	tests/Paginator/KeysetPaginatorTest.php

--- src/Paginator/TokenPaginator.php
+++ src/Paginator/TokenPaginator.php
@@ -4,42 +4,196 @@ declare(strict_types=1);
 namespace Yiisoft\Data\Paginator;
 
 
+use Yiisoft\Cache\Cache;
 use Yiisoft\Data\Reader\DataReaderInterface;
-use Yiisoft\Data\Reader\FilterableDataInterface;
 use Yiisoft\Data\Reader\TokenableDataInterface;
 
-class TokenPaginator
+class TokenPaginator implements PaginatorInterface
 {
+    /**
+     * @var DataReaderInterface
+     */
     private $dateReader;
+    /**
+     * @var PaginatorInterface
+     */
     private $paginator;
-    private $token;
+    /**
+     * @var int|null
+     */
     private $ttl;
+    /**
+     * @var string|null
+     */
+    private $nextToken;
+    /**
+     * @var string|null
+     */
+    private $previousToken;
+    /**
+     * @var int
+     */
+    private $pageSize = self::DEFAULT_PAGE_SIZE;
+    /**
+     * @var mixed
+     */
+    private $currentNextToken;
+    /**
+     * @var mixed
+     */
+    private $currentPreviousToken;
+    /**
+     * @var Cache|null
+     */
+    private $tokenStorage;
+    /**
+     * @var array|null
+     */
+    private $readCache;
 
     public function __construct(DataReaderInterface $dataReader)
     {
         $this->dateReader = $dataReader;
     }
 
-    public function withPaginator($paginator): self
+    /**
+     * @param PaginatorInterface|null $paginator
+     * @return TokenPaginator
+     */
+    public function withPaginator(?PaginatorInterface $paginator)
     {
         $new = clone $this;
         $new->paginator = $paginator;
         return $new;
     }
 
-    public function withToken(string $token): self
+    /**
+     * @param Cache|null $cache
+     * @return TokenPaginator
+     */
+    public function withTokenCache(?Cache $cache)
     {
         $new = clone $this;
-        $new->token = $token;
+        $new->tokenStorage = $cache;
         return $new;
     }
 
-    public function getNextToken(): string {
-        if($this->dateReader instanceof TokenableDataInterface) {
-            return $this->dateReader->getNextToken();
+    public function read(): iterable
+    {
+        if ($this->readCache !== null) {
+            return $this->readCache;
         }
+        if ($this->paginator !== null) {
+            $paginator = $this->paginator
+                ->withPreviousPageToken($this->decodeToken($this->previousToken))
+                ->withNextPageToken($this->decodeToken($this->nextToken))
+                ->withPageSize($this->pageSize);
+            return $paginator->read();
+        }
+        $reader = $this->dateReader->withLimit($this->pageSize);
 
-        // @TODO generate
-        return '';
+        if ($reader instanceof TokenableDataInterface) {
+            $this->currentNextToken = $reader->getNextToken();
+            $this->currentPreviousToken = $reader->getPreviousToken();
+        }
+    }
+
+    public function isOnLastPage(): bool
+    {
+        $this->initialize();
+    }
+
+    public function isOnFirstPage(): bool
+    {
+        $this->initialize();
+    }
+
+    public function getNextPageToken(): ?string
+    {
+        $this->initialize();
+        return $this->encodeToken($this->currentNextToken);
+    }
+
+    public function getPreviousPageToken(): ?string
+    {
+        $this->initialize();
+        return $this->encodeToken($this->currentPreviousToken);
+    }
+
+    /**
+     * @return static
+     */
+    public function withNextPageToken(?string $token)
+    {
+        $new = clone $this;
+        $new->nextToken = $token;
+        if ($token !== null) {
+            $new->previousToken = null;
+        }
+        return $new;
+    }
+
+    /**
+     * @return static
+     */
+    public function withPreviousPageToken(?string $token)
+    {
+        $new = clone $this;
+        $new->previousToken = $token;
+        if ($token !== null) {
+            $new->nextToken = null;
+        }
+        return $new;
+    }
+
+    /**
+     * @return static
+     */
+    public function withPageSize(int $limit)
+    {
+        $new = clone $this;
+        $new->pageSize = $limit;
+        return $new;
+    }
+
+    protected function getTokenStorageData(string $token)
+    {
+        return $this->tokenStorage->get($token);
+    }
+
+    protected function setTokenStorageData(string $token, $data)
+    {
+        return $this->tokenStorage->set($token, $data);
+    }
+
+    protected function encodeToken($data)
+    {
+        return (string)$data;
+    }
+
+    protected function decodeToken(?string $token)
+    {
+        return $token;
+    }
+
+    public function getCurrentPageSize(): int
+    {
+        $this->initialize();
+        return count($this->readCache);
+    }
+
+    protected function initialize()
+    {
+        if ($this->readCache !== null) {
+            return;
+        }
+        foreach ($this->read() as $void) ;
+    }
+
+    public function __clone()
+    {
+        $this->readCache = null;
+        $this->currentPreviousToken = null;
+        $this->currentNextToken = null;
     }
 }
